4fcf54a7a39d689226860cc14daf3d11850607b2
Vasily Postnicov <shamaz.mazum@gmail.com>
2019-11-02 19:31:28 +0300

    Use visibility preset to controll what to export

--- program-map.h
+++ program-map.h
@@ -1,13 +1,15 @@
 #ifndef _PROGRAM_MAP_H_
 #define _PROGRAM_MAP_H_
 
+#define PM_EXPORT __attribute__((visibility("default")))
+
 struct pm_program_handler {
     void *ph_space;;
     size_t ph_size;
 };
 
-int pm_map_program (struct pm_program_handler *handler, const char *path);
-int pm_unmap_program (struct pm_program_handler *handler);
-const char* pm_get_error();
+PM_EXPORT int pm_map_program (struct pm_program_handler *handler, const char *path);
+PM_EXPORT int pm_unmap_program (struct pm_program_handler *handler);
+PM_EXPORT const char* pm_get_error();
 
 #endif
