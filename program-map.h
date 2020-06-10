cdc1509538f98f7d4153bd734814af79b3a508a3
Vasily Postnicov <shamaz.mazum@gmail.com>
2020-06-10 21:14:09 +0300

    Now can be used with C++

--- program-map.h
+++ program-map.h
@@ -3,6 +3,10 @@
 
 #define PM_EXPORT __attribute__((visibility("default")))
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 struct pm_program_handler {
     void *ph_space;;
     size_t ph_size;
@@ -11,5 +15,9 @@ struct pm_program_handler {
 PM_EXPORT int pm_map_program (struct pm_program_handler *handler, const char *path);
 PM_EXPORT int pm_unmap_program (struct pm_program_handler *handler);
 PM_EXPORT const char* pm_get_error();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
